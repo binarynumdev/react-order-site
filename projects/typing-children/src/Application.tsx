50624cb2538070712a359ab608d9843d7c35c4dc
binarynumdev <n.origin8705@gmail.com>
2023-12-21 23:03:46 -0800

    testing

--- projects/typing-children/src/Application.tsx
+++ projects/typing-children/src/Application.tsx
@@ -24,9 +24,6 @@
        Just a string.
        <p>Some HTML that is not nested.</p>
        <Box>
-         <h2>Another React component with one child.</h2>
-       </Box>
-       <Box>
          <h2>A nested React component with two children.</h2>
          <p>The second child.</p>
        </Box>
